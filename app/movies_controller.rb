2046c92c6ac512509a00c73159f9a3f36f114109
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 19:49:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block(args_block = {title: "Home Alone", release_date: 2003})
+def can_be_created_in_a_block(args_block = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
