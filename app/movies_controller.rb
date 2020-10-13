390fc000dc4672b768a48da60f1aacc930a2175d
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 19:30:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,8 +30,8 @@ def can_be_created_in_a_block(args_block = {title: "The Room", release_date: 200
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title 
+    m.release_date 
   end
 end
 
